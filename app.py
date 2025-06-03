7eb0d0bd00ead9d19e66233845bd3d5cdf79415f
felix <thaofami@gmail.com>
2025-06-03 17:40:44 +0700

    fd

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def search():
                            search_text=search_text)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True,host='0.0.0.0', port=8000)
